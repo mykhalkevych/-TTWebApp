5a2ae47b987ac165fccda4fb09b53fcfc4e554b3
nazik <nmykhalkevych@ukrinsoft.net>
2018-12-04 20:49:43 +0200

    added preloader on app start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ttwebapp';
+
+  ngOnInit() {
+    const pagePreloader = document.querySelector('.page-preloader');
+    setTimeout(() => {
+      pagePreloader.classList.remove('loading');
+    }, 300);
+  }
 }
