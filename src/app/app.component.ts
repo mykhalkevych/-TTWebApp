d35db4441819ea7d76e8ea86d833e775815cfa2a
nazik542 <nazik542@gmail.com>
2019-04-23 22:41:34 +0300

    update bitbucket pipelines

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ttwebapp';
+<<<<<<< Updated upstream
+=======
+  isLoading: Observable<boolean>;
+  message;
+
+  constructor(
+    private store: Store<AppState>,
+    private messagingService: MessagingService
+  ) {
+
+  }
+  ngOnInit() {
+    const userId = 'user002';
+    this.messagingService.requestPermission(userId);
+    this.messagingService.receiveMessage();
+    this.message = this.messagingService.currentMessage;
+    const pagePreloader = document.querySelector('.page-preloader');
+    setTimeout(() => {
+      pagePreloader.classList.remove('loading');
+    }, 300);
+    this.isLoading = this.store.select(getIsLoading);
+  }
+>>>>>>> Stashed changes
 }
